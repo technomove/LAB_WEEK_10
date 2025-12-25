8db800285761988bae3775fd9b83620a1b4d529a
Charlie <charliemartin190904@gmail.com>
2025-12-26 06:13:33 +0700

    Commit 1 – add ViewModel

---
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lab_week_10
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val totalViewModel = ViewModelProvider(this)[TotalViewModel::class.java]
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
